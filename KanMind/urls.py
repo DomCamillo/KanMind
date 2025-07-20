22ca056b6a9dcfa8d87c63c180e4ca15f1e59c19
DomCamillo <159430752+DomCamillo@users.noreply.github.com>
2025-07-20 16:07:06 +0200

    added Models

--- KanMind/urls.py
+++ KanMind/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path ,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/Board/', include('Kan_Mind_app.api.urls')),
-
+    path("admin/", admin.site.urls),
+    path("api/", include("Kan_Mind_app.api.urls")),
+    path("api-auth/", include("rest_framework.urls")),
 ]
--- Kan_Mind_app/models.py
+++ Kan_Mind_app/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Board (models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_baords")
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+class BoardUser(models.Model):
+
+    ROLE_CHOICES = (
+        ("owner", "Owner"),
+        ("member", "Member"),
+        ("reviewer", "Reviewer")
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="board_member")
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="members")
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="member")
+
+    def __str__(self):
+        return self.user
+
+
+
+class Column (models.Model):
+    baord = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='columns')
+    title = models.CharField(max_length=255)
+    position = models.PositiveBigIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.title} ({self.baord.title})"
+
+
+
+
+class Task (models.Model):
+    column = models.ForeignKey(Column, related_name='tasks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    position = models.PositiveBigIntegerField(default=0)
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='assigned_tasks', null=True, blank=True)
+    due_date = models.DateTimeField(auto_now_add=True)
+    urgency = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Comment (models.Model):
+    task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.task} {self.author}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Create your models here.
